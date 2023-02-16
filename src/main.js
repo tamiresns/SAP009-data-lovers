65e1261fb80f4dec6e3e006dc0c95e784ed3b1dd
tamiresns <tamiresmn@live.com>
2023-02-16 16:05:32 -0300

    Concluindo a ordenação Z-A

--- src/main.js
+++ src/main.js
@@ -28,19 +28,64 @@ function mostrar(){
   `)
 }
 
-const ordemAlfabetica = document.getElementById("ordemAlfabetica")
-ordemAlfabetica.addEventListener("change", ordenar)
-
-
-function ordenar(){
-  data.sort(function(a,b){
-    if(a.name < b.name){
-      return -1;
-    }
-    else{
-      return true;
-    }
-  })
+function mostrarFiltrado(event){
+  const root = document.getElementById('container-personagens')
+  root.innerHTML = ordenar(data.results, event.target.value).map((personagemAtual) => 
+    `<div>
+      <img src="${personagemAtual.image}" alt="Imagem do Personagem">
+      <ul class="listaDados">
+        <li class="linhaEscura">NOME:</li>
+        <li class="linhaEscura">${personagemAtual.name}</li>
+        <li class="linhaClara">ESTADO DE VIDA:</li>
+        <li class="linhaClara">${personagemAtual.status}</li>
+        <li class="linhaEscura">GÊNERO:</li>
+        <li class="linhaEscura">${personagemAtual.gender}</li>
+        <li class="linhaClara">LOCAL DE ORIGEM:</li>
+        <li class="linhaClara">${personagemAtual.origin.name}</li>
+        <li class="linhaEscura">LUGAR ONDE VIVE:</li>
+        <li class="linhaEscura">${personagemAtual.location.name}<li>
+        <li class="linhaClara">EPISÓDIOS EM QUE APARECE:</li>
+      </ul>
+    </div>
+`)
 }
+
+const ordemAlfabetica = document.getElementById("ordemAlfabetica");
+ordemAlfabetica.addEventListener("change", mostrarFiltrado);
+
+
+function ordenar(dadosFiltrados, tipoOrdenacao){
+  const novaArray = [...dadosFiltrados]
+
+  if(tipoOrdenacao === "az") {
+    novaArray.sort(function(a,b){
+      if(a.name < b.name){
+        return -1;
+      }
+      else{
+        return +1;
+      }
+    })
+  } 
+  else if(tipoOrdenacao === "za"){ //exbibe na ordem inversa
+    novaArray.sort(function(a,b){
+      if(a.name > b.name){
+        return -1;
+      }
+      else{
+        return +1;
+      }
+    })
+  }
+  return novaArray;
+  /*if(ordem.target.value === "az"){
+    return novaArray;
+  }
+  else if(ordem.target.value === "za"){
+    return novaArray.reverse();
+  }*/
+}
+
+
 export default mostrar
 
